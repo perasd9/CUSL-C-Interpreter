9128ab6aa1c3b5ab78254265f865fd36992e948e
pera <nikodijevicp01@gmail.com>
2024-12-04 23:08:31 +0100

    added binary ops

--- src/main.c
+++ src/main.c
@@ -14,11 +14,26 @@ int main(int argc, char *argv[]) {
 	
 	initChunk(&chunk);
 	
-	int constant = addConstant(&chunk, 1.2);
-	
+	int constant = addConstant(&chunk, 1);
 	
 	writeChunk(&chunk, OP_CONSTANT, 10);
 	writeChunk(&chunk, constant, 10);
+	
+	constant = addConstant(&chunk, 3);
+	
+	writeChunk(&chunk, OP_CONSTANT, 10);
+	writeChunk(&chunk, constant, 10);
+	
+	writeChunk(&chunk, OP_ADD, 10);
+	
+	constant = addConstant(&chunk, 8);
+	
+	writeChunk(&chunk, OP_CONSTANT, 10);
+	writeChunk(&chunk, constant, 10);
+	
+	writeChunk(&chunk, OP_DIVIDE, 10);
+	
+	writeChunk(&chunk, OP_NEGATE, 123);
 	
 	writeChunk(&chunk, OP_RETURN, 10);
 	
