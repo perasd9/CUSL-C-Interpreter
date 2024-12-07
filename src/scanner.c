4be7330029b2ac9c12d721e3c2f73c49cb639cc7
pera <nikodijevicp01@gmail.com>
2024-12-07 23:31:39 +0100

    started scanning

--- src/scanner.c
+++ src/scanner.c
@@ -17,3 +17,41 @@ void initScanner(const char* source) {
 	scanner.current = source;
 	scanner.line = 1;
 }
+
+bool isAtEnd() {
+	return *scanner.current == '\0';
+}
+
+Token makeToken(TokenType type) {
+	Token token;
+	
+	token.start = scanner.start;
+	token.length = (int)(scanner.current - scanner.start);
+	
+	token.type = type;
+	token.line = scanner.line;
+	
+	return token;
+}
+
+Token errorToken(const char* message) {
+	Token token;
+	token.type = TOKEN_ERROR;
+	
+	token.start = message;
+	token.length = (int)strlen(message);
+	
+	token.line = scanner.line;
+	
+	return token;
+}
+
+Token scanToken() {
+	scanner.start = scanner.current;
+	
+	if(isAtEnd()) return makeToken(TOKEN_EOF);
+	
+	return errorToken("Unexpected character");
+}
+
+
