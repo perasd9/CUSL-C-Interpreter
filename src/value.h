098010dceabe73a6baaadd4fe1935c499b8fc0f9
pera <nikodijevicp01@gmail.com>
2024-12-25 19:41:19 +0100

    added cusl types

--- src/value.h
+++ src/value.h
@@ -3,7 +3,30 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+	VALUE_BOOL,
+	VALUE_NIL,
+	VALUE_NUMBER
+} ValueType;
+
+typedef struct {
+	ValueType type;
+	union {
+		bool boolean;
+		double number;
+	} as;
+} Value;
+
+#define IS_BOOL(value) ((value.type) == VALUE_BOOL)
+#define IS_NIL(value) ((value.type) == VALUE_NIL)
+#define IS_NUMBER(value) ((value.type) == VALUE_NUMBER)
+
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+#define BOOL_VAL(value) ((Value){VALUE_BOOL, {.boolean = value}})
+#define NIL_VAL ((Value){VALUE_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VALUE_NUMBER, {.number = value}})
 
 typedef struct {
 	int capacity;
